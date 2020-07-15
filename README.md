56794e3139b41726605492379b6ed1d259f44606
Amara-crypto <thomastarawali@gmail.com>
2020-07-15 16:00:51 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#REACT Shoping app.
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
